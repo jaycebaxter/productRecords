48d06c62dc69f66ab9ffcde6cbd08c653e447700
jayce <jayce.baxter@proton.me>
2025-03-26 04:26:28 -0400

    init

---
+++ Product.java
@@ -0,0 +1,21 @@
+public class Product {
+
+    // The ID / SKU of the product
+    public int skuNum;
+
+    // The name of the product
+    public String productName;
+
+    // The cost of the product
+    public double productCost;
+
+    // Number in stock
+    public int inStock;
+
+    // Amount we need to order
+    public int toOrder;
+
+    // Special instructions
+    public String specialInstructions;
+
+}

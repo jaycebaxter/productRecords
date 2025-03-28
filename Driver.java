9588f9c8b9c60d5b76f6c1655bc4e3554d61971b
jayce <jayce.baxter@proton.me>
2025-03-28 00:48:13 -0400

    added driver / product

---
+++ Driver.java
@@ -0,0 +1,21 @@
+public class Driver {
+    public static void driver(String[] args) {
+
+        // Canned Beans product that doesn't expire
+        Product cannedBeans = new Product();
+
+        cannedBeans.setSkuNum(100629175);
+        cannedBeans.setProductName("Canned Beans");
+        cannedBeans.setProductCost(5.99);
+        cannedBeans.setInStock(16);
+        cannedBeans.setToOrder(3);
+        cannedBeans.setSpecialInstructions("BE CAREFUL!!!!!!!!!!!!!!");
+
+        // Canned corn that doesn't expire
+
+        Product cannedCorn = new Product();
+
+    }
+}
+
+

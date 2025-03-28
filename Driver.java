df1f62063709a70f1449d920e936293eb1eb8765
jayce <jayce.baxter@proton.me>
2025-03-28 00:53:29 -0400

    why was i using 8 lines of setters

--- Driver.java
+++ Driver.java
@@ -2,14 +2,7 @@ public class Driver {
     public static void driver(String[] args) {
 
         // Canned Beans product that doesn't expire
-        Product cannedBeans = new Product();
-
-        cannedBeans.setSkuNum(100629175);
-        cannedBeans.setProductName("Canned Beans");
-        cannedBeans.setProductCost(5.99);
-        cannedBeans.setInStock(16);
-        cannedBeans.setToOrder(3);
-        cannedBeans.setSpecialInstructions("BE CAREFUL!!!!!!!!!!!!!!");
+        Product cannedBeans = new Product(100629175, "Canned Beans", 5.99, 16, 3, "BE CAREFUL!!!!!!!!!!!!!!");
 
         // Canned corn that doesn't expire
 
